05e9fd7fec6230a420e53d77b2d6dc960a9a197b
TimFau <tim@timfau.com>
2020-01-01 19:39:24 -0500

    Homepage Design Refresh

--- front-page.php
+++ front-page.php
@@ -45,31 +45,35 @@
 <?php
     $args = array(
         'post_type' =>  'post',
-        'posts_per_page'   => 4,
+        'posts_per_page'   => 3,
         'post__not_in' => $do_not_duplicate,
         'category__not_in'  => array( 5 )
     );
     $query_rn = new WP_Query( $args );
 ?>
 <div class="not_featured">
-<section class="recentnews l_container">
+<section class="recentnews l_container design-1">
     <div class="headingbox">
         <h3 class="hpheading hpheading_hp">Recent Content<i class="material-icons dhide">keyboard_arrow_down</i></h3>
     </div>
     <?php if( $query_rn->have_posts() ) : while( $query_rn->have_posts() ) : $query_rn->the_post(); ?>
-    <div class="recent_item">
-                <div class="ri_img">
-                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
-                </div>
-                <div class="recent_desc">
-                    <div class="postcategory">
-                        <?php the_category(' '); ?>
-                    </div>
-                    <a href="<?php the_permalink(); ?>">
-                        <h2><?php the_title(); ?></h2>
-                    </a>
-                    <p><?php echo get_excerpt(); ?></p>
-                </div>
+    <div class="item recent_item">
+        <div class="ri_img">
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
+        </div>
+        <div class="desc recent_desc">
+            <div class="postcategory">
+                <?php the_category(' '); ?>
+            </div>
+            <a href="<?php the_permalink(); ?>">
+                <h2><?php the_title(); ?></h2>
+                <?php 
+                    $ft_short_desc = wp_trim_words( get_the_content(), 40, '...' );
+                ?>
+                <p><?php echo $ft_short_desc; ?></p>
+                <button class="read-more">Read More</button>
+            </a>
+        </div>
     </div>
     <?php endwhile; endif; wp_reset_postdata(); ?>
 </section>
@@ -85,17 +89,28 @@
     
 <!-- New Releases -->
 <section class="newreleases l_container">
+    <div class="container">
         <div class="headingbox">
             <a href="category/albums/">
             <h3 class="hpheading hpheading_hp">Album Spotlight<i class="material-icons dhide">keyboard_arrow_down</i></h3>
             </a>
         </div>
-        <?php if( $query_nr->have_posts() ) : while( $query_nr->have_posts() ) : $query_nr->the_post(); ?>
-        <div class="album">
+        <?php $i = 0; if( $query_nr->have_posts() ) : while( $query_nr->have_posts() ) : $query_nr->the_post(); $i++; ?>
+        <div class="album <?php echo 'ct-' . $i; ?>">
             <div class="albumimg"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a></div>
-            <div class="albumdesc"><a href="<?php the_permalink(); ?>"><h2>view</h2></a></div>
+            <div class="albumdesc">
+                <a href="<?php the_permalink(); ?>">
+                    <h2><?php the_title(); ?></h2>
+                    <?php 
+                        $ft_short_desc = wp_trim_words( get_the_content(), 20, '...' );
+                    ?>
+                    <p><?php echo $ft_short_desc; ?></p>
+                    <button class="read-more">Read More</button>
+                </a>
+            </div>
         </div> 
         <?php endwhile; endif; wp_reset_postdata(); ?>
+    </div>
 </section>
     
 <?php
@@ -109,22 +124,34 @@
     $query_ua = new WP_Query( $args );
 ?>
 <!-- Upcomming artists -->
-<section class="upcoming l_container">
+<section class="upcoming l_container design-1">
+    <div class="container">
         <div class="headingbox">
             <a href="category/artist-spotlight/">
             <h3 class="hpheading hpheading_hp">Artist Spotlight<br class="dhide"><i class="material-icons dhide">keyboard_arrow_down</i></h3>
             </a>
         </div>
         <?php if( $query_ua->have_posts() ) : while( $query_ua->have_posts() ) : $query_ua->the_post(); ?>
-        <div class="upcomingartist">
-            <div class="ua_img">
+        <div class="item upcomingartist">
+            <div class="ri_img">
                 <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
             </div>
-            <div class="ua_desc">
-                <a href="<?php the_permalink(); ?>"><h2 class="box"><?php the_title(); ?></h2></a>
+            <div class="desc recent_desc">
+                <div class="postcategory">
+                    <?php the_category(' '); ?>
+                </div>
+                <a href="<?php the_permalink(); ?>">
+                    <h2><?php the_title(); ?></h2>
+                    <?php 
+                        $ft_short_desc = wp_trim_words( get_the_content(), 40, '...' );
+                    ?>
+                    <p><?php echo $ft_short_desc; ?></p>
+                    <button class="read-more">Read More</button>
+                </a>
             </div>
         </div>
         <?php endwhile; endif; wp_reset_postdata(); ?>
+    </div>
 </section>
 </div>
 
--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <head>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.typekit.net/nyy8ptf.css">
     <script>/* (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TB66G36'); */</script>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
